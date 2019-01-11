cc8553178f3f3423254efc5a37c6a83962f6a040
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-11 11:22:21 -0600

    displaying all basic info from character data onto the screen

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -17,6 +17,24 @@ function Character (props){
                 <li>{props.character.gender}</li>
                 <li>{props.character.hair_color}</li>
                 <li>{props.character.height}</li>
+                <li>{props.character.homeworld}</li>
+                <li>{props.character.mass}</li>
+                <li>{props.character.skin_color}</li>
+                <ul className="character-species">
+                    {props.character.species.map((sp, index) => {
+                        return <li key={index}>{sp}</li>
+                    })}
+                </ul>
+                <ul className="character-starships">
+                    {props.character.starships.map((ship, index) => {
+                        return <li key={index}>{ship}</li>
+                    })}
+                </ul>
+                <ul className="character-vehicles">
+                    {props.character.vehicles.map((v, index) => {
+                        return <li key={index}>{v}</li>
+                    })}
+                </ul>
             </ul>
         </div>
     );
