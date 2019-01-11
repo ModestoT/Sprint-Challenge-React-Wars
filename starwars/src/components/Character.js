7b698431ea7ae263c65313a2a8242c68fb0512d3
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-11 11:11:53 -0600

    Added more info for each star wars characters. Looped through the characters array of films and displayed them as a list

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -9,10 +9,14 @@ function Character (props){
                 <li>{props.character.created}</li>
                 <li>{props.character.edited}</li>
                 <li>{props.character.eye_color}</li>
-                <li>{props.character.films}</li>
-                <li>{props.character.eye_color}</li>
-                <li>{props.character.eye_color}</li>
-                <li>{props.character.eye_color}</li>
+                <ul className="character-films">
+                    {props.character.films.map((film, index) => {
+                        return <li key={index}>{film}</li>
+                    })}
+                </ul>
+                <li>{props.character.gender}</li>
+                <li>{props.character.hair_color}</li>
+                <li>{props.character.height}</li>
             </ul>
         </div>
     );
