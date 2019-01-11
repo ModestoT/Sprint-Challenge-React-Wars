525a921cdc85d056335fd243af1ca755b2168580
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-11 12:26:43 -0600

    Added some more styling to the List element for each character

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -7,14 +7,14 @@ function Character (props){
             <div className="character-name">{props.character.name}</div>
             <ul className="character-info">
                 <li>Birth Year: {props.character.birth_year}</li>
-                <li>Created: {props.character.created}</li>
-                <li>Edited: {props.character.edited}</li>
                 <li>Eye Color: {props.character.eye_color}</li>
                 <li>Gender: {props.character.gender}</li>
                 <li>Hair Color: {props.character.hair_color}</li>
                 <li>Height: {props.character.height}</li>
                 <li>Mass: {props.character.mass}</li>
                 <li>Skin Color: {props.character.skin_color}</li>
+                <li>Created: {props.character.created}</li>
+                <li>Edited: {props.character.edited}</li>
             </ul>
         </div>
     );
